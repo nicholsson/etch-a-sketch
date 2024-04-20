ecf6980153fa229e61c42f5343ab5c0ae9042800
Nicola <nicoanta95@gmail.com>
2024-04-20 19:00:41 +0200

    Created grid

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // create a webpage with a 16x16 grid of square divs
+const board = document.querySelector("#container");
+for (let i = 0; i < 256; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    board.appendChild(square);
+};
