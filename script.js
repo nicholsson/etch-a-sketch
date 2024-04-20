df4286752ac7c4efa29cc667d16839519da5405a
Nicola <nicoanta95@gmail.com>
2024-04-20 17:49:30 +0200

    gotta do things well

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
 // create a webpage with a 16x16 grid of square divs
-const matrix = document.querySelector("#matrix");
-for (let i = 0; i < 32; i++) {
-    const grid_element = document.createElement("div");
-    div.classList.add("grid");
-    matrix.appendChild(grid_element);
-};
