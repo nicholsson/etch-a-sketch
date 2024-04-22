1b4706dfeb9ba22d5d28b57aec5db87e1327a012
nicholsson <nicoanta95@gmail.com>
2024-04-22 11:27:43 +0200

    Added global variables and retouched createGrid + small editings

--- script.js
+++ script.js
@@ -1,54 +1,45 @@
-// little headache: squares contains a "list" of the Nodes with class .square
-// so it is necessary to access that list with a for loop to add an event listener
-function mouseOvering() {
-    const squares = document.querySelectorAll(".square");
-    squares.forEach(square => {
-        square.addEventListener("mouseover", () => {
-            square.style.backgroundColor = "blue";
-        })
-    });
-};
-function createGrid(size){
-    const board = document.querySelector("#container");
-    const numberOfSquares = size * size;
+// global variables here
+let defaultSize = 16;
+let numberOfSquares = Math.pow(defaultSize, 2);
+let size = defaultSize;
+const board = document.querySelector("#container");
+const resizeButton = document.querySelector("#size");
+const resetButton = document.querySelector("#reset");
+// more global variables need to be added
+// here
+//
+function createGrid(){
+    numberOfSquares = size * size;
     const squareSize = 800.0 / size;
     //clear the board before creating the grid
     board.innerHTML = ""; 
     for (let i = 0; i < numberOfSquares; i++) {
-        const square = document.createElement("div");
+	const square = document.createElement("div");
         square.classList.add("square");
         square.style.width = squareSize + "px";
         square.style.height = squareSize + "px";
         board.appendChild(square);
-        // call the coloring effect on mouse hovering after grid is created
-        mouseOvering();
+        // call the coloring effect on mouse hovering on each square
+        square.addEventListener("mouseover", () => {
+	square.style.backgroundColor = "blue";
+	});
+	// call the reset effect on each square when resetButton is clicked
+	resetButton.addEventListener("click", () => {
+	square.style.backgroundColor = "white";});
     };
 }
 // Size button behavior: assign custom grid size
 function resizeGrid(){
-    const sizeButton = document.querySelector("#size");
-    sizeButton.addEventListener("click", () => {
+    resizeButton.addEventListener("click", () => {
         let newSize = prompt("insert size: ");
         newSize = parseInt(newSize);
-        if (Number.isInteger(newSize) && newSize > 0) {
+        if (Number.isInteger(newSize) && newSize > 0 && newSize < 100) {
             size = newSize;
-            createGrid(size);
-            resetGrid();
+            createGrid();
         } else {
-            alert("please enter an integer");
+            alert("please enter an integer between 0 and 100");
         }});
 }
-// When done drawing, reset the grid
-function resetGrid(){
-    const resetButton = document.querySelector("#reset");
-    const squares = document.querySelectorAll(".square");
-    resetButton.addEventListener("click", () => {
-        squares.forEach(square => {
-            square.style.backgroundColor = "white";
-        })
-    })
-};
 // create a 16x16 grid by default:
-createGrid(16);
-resetGrid();
+createGrid();
 resizeGrid();
